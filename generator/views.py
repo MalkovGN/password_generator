02a54157798531b546fe2b1212c7e770947a0e02
MalkovGN <malkovgn@gmail.com>
2022-03-05 21:00:52 +0300

    Deleting an old func

--- generator/views.py
+++ generator/views.py
@@ -10,14 +10,6 @@ def home(request):
     return render(request, "generator/home.html")
 
 
-def page1(request):
-    return HttpResponse('<h1>This is the first page of my site!</h1>')
-
-
-def page2(request):
-    return HttpResponse('<h1>This is the second page of my site!</h1>')
-
-
 def password(request):
     characters = list(string.ascii_lowercase)
 
